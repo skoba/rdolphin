d4ab50cecd9bdd84d241a2e4fb0ddb185441a38e
Shinji KOBAYASHI <skoba@moss.gr.jp>
2014-02-28 05:34:27 +0900

    ajax is difficult

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,6 @@ Rdolphin::Application.configure do
   config.log_level = :debug
   config.cache_classes = false
   config.eager_load = false
-
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
@@ -11,7 +10,5 @@ Rdolphin::Application.configure do
   config.active_support.deprecation = :log
 
   config.active_record.migration_error = :page_load
-  
-
   config.assets.debug = true
 end
