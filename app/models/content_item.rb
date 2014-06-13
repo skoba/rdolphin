5e8a815a3c846a44c2b81c48155309773bde6038
Shinji KOBAYASHI <skoba@moss.gr.jp>
2014-06-13 13:25:04 +0900

    merge seeding

--- app/models/content_item.rb
+++ app/models/content_item.rb
@@ -1,4 +1,6 @@
 class ContentItem < ActiveRecord::Base
   belongs_to :composition
 
+  validates :rm_type_name, presence: true
+  validates :archetype_id, presence: true
 end
--- spec/models/content_item_spec.rb
+++ spec/models/content_item_spec.rb
@@ -1,3 +1,19 @@
 RSpec.describe ContentItem, :type => :model do
   it {is_expected.to belong_to :composition}
+
+  let(:content_item) { build :content_item }
+
+  it 'is valid with built option' do
+    expect(content_item).to be_valid
+  end
+
+  it 'is not valid without rm_type_name' do
+    content_item.rm_type_name = nil
+    expect(content_item).to have(1).error_on(:rm_type_name)
+  end
+
+  it 'is not valid without archetype_id' do
+    content_item.archetype_id = ''
+    expect(content_item).to have(1).error_on(:archetype_id)
+  end
 end
