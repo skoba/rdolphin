c2c470a1cc4531a9f9e640943149291abed3c445
Shinji KOBAYASHI <skoba@moss.gr.jp>
2014-06-14 19:12:44 +0900

    add ehr controlelr spec

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Rdolphin
       fixtures: true,
       view_specs: false,
       routing_specs: false,
+      helper_specs: false,
       controller_specs: true,
       request_specs: false
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
--- spec/controllers/ehr_controller_spec.rb
+++ spec/controllers/ehr_controller_spec.rb
@@ -1,5 +1,3 @@
-require 'rails_helper'
-
 RSpec.describe EhrController, :type => :controller do
 
   describe "GET 'index'" do
@@ -7,6 +5,10 @@ RSpec.describe EhrController, :type => :controller do
       get 'index'
       expect(response).to be_success
     end
+
+    it 'renders the :index template'
+
+    it 'populates an array of ehrs in the repository '
   end
 
   describe "GET 'show'" do
@@ -14,6 +16,8 @@ RSpec.describe EhrController, :type => :controller do
       get 'show'
       expect(response).to be_success
     end
-  end
 
+    it 'assigns the requested composition to @ehr'
+    it 'renders the :show template'
+  end
 end
