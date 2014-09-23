645b414f3c6dfd59d4eb0a3a367112fb1352e2e7
Shinji KOBAYASHI <skoba@moss.gr.jp>
2014-09-23 18:39:57 +0900

    result list

--- spec/features/mml_lab_tests_spec.rb
+++ spec/features/mml_lab_tests_spec.rb
@@ -1,7 +1,8 @@
 feature 'MML Lab tests' do
-  scenario 'list lab tests' do
+  scenario 'list of lab test' do
     visit mml_lab_tests_path
 
     expect(page).to have_title 'MML Lab test result'
+    expect(page).to have_content 'Date'
   end
 end
